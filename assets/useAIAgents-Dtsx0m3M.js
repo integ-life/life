531b034277e8fdc53a200547ee31836d79b19c4b
Song Yangyu <flyfy1@gmail.com>
2026-09-19 10:08:32 +0800

    Updates

---
+++ assets/useAIAgents-Dtsx0m3M.js
@@ -0,0 +1,1 @@
+import{r as e,u as t}from"./vendor-DwUwVCXp.js";import{A as s}from"./index-Di4oN7Nk.js";const r=()=>{const[r,n]=e.useState([]),[a,o]=e.useState(null),u=t(e=>e.isOfflineMode),f=u?[]:r;return e.useEffect(()=>{if(u)return;let e=!0;return s.listAIAgents().then(t=>{e&&n(t.agents||[])}).catch(t=>{e&&o(t.message)}),()=>{e=!1}},[u]),{agents:f,error:a}};export{r as u};
